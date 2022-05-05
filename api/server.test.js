20519e07567baa949eb5eead2f094c433b952023
William Lee <leewp8@gmail.com>
2022-05-05 00:45:48 -0400

    testing in place

--- api/server.test.js
+++ api/server.test.js
@@ -1,7 +1,7 @@
 const server = require('./server')
 const request = require('supertest')
 const db = require('../data/dbConfig')
-const authRouter = require('./auth/auth-router')
+const router = require('./auth/auth-router')
 
 
 beforeAll(async () => {
@@ -21,13 +21,29 @@ afterAll(async () => {
 
 describe('[POST] register user', () => {
   test('registering new user returns a status 201', async () => {
-    let res = await request(authRouter).post('/register').send({ username: 'montana', password: 'ranch' });
+    let res = await request(router).post('/register').send({ username: 'montana', password: 'ranch' });
     expect(res.status).toBe(201);
+    expect(res.message).toBe('welcome, montana')
 
-    test('sending body without username or password return error', async () => {
-      res = await request(authRouter).post('/register').send({});
+    test('sending body without username or password returns error', async () => {
+      res = await request(router).post('/register').send({});
       expect(res.status).toBe(500);
       expect(res.message).toBe('username and password required')
     })
   });
 })
+
+
+describe('[POST] login user', () => {
+  test('logging in user correctly responds with status 200', async () => {
+    let res = await request(router).post('/login').send({ username: 'montana', password: 'ranch' });
+    expect(res.status).toBe(200);
+
+    test('sending body without username or password returns error', async () => {
+      res = await request(router).post('/register').send({});
+      expect(res.status).toBe(500);
+      expect(res.message).toBe('username and password required')
+    })
+  });
+})
+
